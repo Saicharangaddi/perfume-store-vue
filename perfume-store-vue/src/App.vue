a3aaa1361f9f2389fe3fc4dc93becb703dc80073
Sai charan <saicharangaddi@gmail.com>
2025-10-02 06:25:16 +0530

    Set up root App component

---
+++ perfume-store-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <Hero />
+    <router-view />
+    <Products />
+    <Categories />
+    <Offers />
+    <About />
+    <Contact />
+    
+  </div>
+  </template>
+
+  <script setup>
+  import Header from './components/Header.vue';
+  import Hero from './components/Hero.vue';
+  import Products from './components/Products.vue';
+  import Categories from './components/Categories.vue';
+  import Offers from './components/Offers.vue';
+  import About from './components/About.vue';
+  import Contact from './components/Contact.vue';
+  </script>
+
