e03457b45b2c59406f64241b8f822e2b2f76d22e
Sai charan <saicharangaddi@gmail.com>
2025-10-07 23:25:49 +0530

    Test: Add comment to Header.vue

--- perfume-store-vue/src/components/Header.vue
+++ perfume-store-vue/src/components/Header.vue
@@ -1,10 +1,11 @@
 <template>
     <header class="sticky top-0 z-50 transition-all duration-300">
-        <section :class="[w-full, isScrolled ? 'bg-gray-100/95 backdrop blur shadow-md py-2':'bg-gray-100 py-4']">
+     <section :class="[w-full, isScrolled ? 
+     'bg-gray-100/95 backdrop blur shadow-md py-2':'bg-gray-100 py-4']">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div 
-                class="flex flex-col md:flex-row items-center justify-between gap-3  md:gap-6"
-              >
+                class="flex flex-col md:flex-row items-center 
+                justify-between gap-3  md:gap-6">
                 <!--Logo Mobile Menu Toggle-->
                 <div class="flex justify-between items-center w-full md:w-auto">
                   <a href="/" class="text-2xl font-bold text-pink-600">ShopEase</a>
@@ -18,15 +19,17 @@
                 </div>
 
                 <!--Search Bar-->
-                 <form class="w-full md:flex-1 max-w-sm" role="search" aria-label="Site search">
+                 <form class="w-full md:flex-1 max-w-sm" role="search"
+                  aria-label="Site search">
                    <label class="relative w-full">
                       <input type="text" placeholder="Search ..."
-                      class="w-full px-3 py-2 text-xs border border-gray-300 rounded-full focus:outline none 
+                      class="w-full px-3 py-2 text-xs border border-gray-300 
+                      rounded-full focus:outline none 
                       focus:ring-2 focus:ring-indigo-500">
 
                       <button type="submit"
-                      class="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-indigo600"
-                      aria-label="Search button"
+                      class="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500
+                       hover:text-indigo600" aria-label="Search button"
                       >
                       <Icon icon="mdi:magnify" class="w-4 h-4" />
                   </button>
@@ -35,23 +38,23 @@
 
                  <!--Icons-->
                   <aside class="flex items-center justify-end space-x-4 w-full md:w-auto">
-                   <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
+                   <button class="relative p-2 text-gray-700 hover:text-pink-600" 
+                   aria-label="Wishlist">
                        <Icon icon="mdi:heart-outline" class="w-5 h-5" />
                        <span
-                       class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
-                       flex items-center justify-center">3</span>
+                       class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs 
+                       rounded-full h-5 w-5 flex items-center justify-center">3</span>
                    </button>
                    <button 
-                   @click="isCartOpen = !isCartOpen" class="relative p-2 text-gray-700 hover:text-pink-600" 
-                   aria-label="Cart">
+                   @click="isCartOpen = !isCartOpen" class="relative p-2 text-gray-700 
+                   hover:text-pink-600" aria-label="Cart">
                    <Icon icon="mdi:cart-outline" class="w-5 h-5" />
-                   <span class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5 flex 
-                   items-center justify-center">{{ cart.cartCount }}
+                   <span class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs
+                    rounded-full h-5 w-5 flex items-center justify-center">{{ cart.cartCount }}
                    </span>
                   </button>
-
-
-                   <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
+                   <button class="relative p-2 text-gray-700 hover:text-pink-600" 
+                   aria-label="Wishlist">
                        <Icon icon="mdi:account-outline" class="w-5 h-5" />
                    </button>
                   </aside>
@@ -62,14 +65,17 @@
         <!--Navbar Links-->
          <nav class="bg-pink-950" aria-label="Main navigation">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <ul class="hidden md:flex justify-center py-3 flex-wrap gap-x-6 text-sm font-medium text-white">  
+                <ul class="hidden md:flex justify-center py-3 flex-wrap gap-x-6 text-sm 
+                font-medium text-white">  
                     <li v-for="item in navItems" :key="item.id">
-                        <a :href="item.link" class="hover:text-pink-300 transition-colors">{{item.name}}</a>
+                        <a :href="item.link" class="hover:text-pink-300 transition-colors">
+                            {{item.name}}</a>
                     </li>  
                 </ul>
 
                 <section v-if="isMobileMenuOpen"
-                class="md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 text-[#5D4037] text-center"
+                class="md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 
+                text-[#5D4037] text-center"
                 aria-label="Mobile Navigation">
                 <a v-for="item in navItems" :href="item.link" :key="item.id"
                 class="block hover:text-amber-600 text-sm font-medium">{{ item.name }}</a>
