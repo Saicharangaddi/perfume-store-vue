d6f0230375555b464ab2ae3458ff5623fe46aa39
Sai charan <saicharangaddi@gmail.com>
2025-10-15 03:55:52 +0530

    Update router.js with protected routes and Welcome.vue entry point

--- perfume-store-vue/src/router.js
+++ perfume-store-vue/src/router.js
@@ -2,6 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+  path: '/home',
+  name: 'Home',
+  component: () => import('../src/Home.vue')
+ },
+  {
+  path: '/',
+  name: 'Welcome',
+  component: () => import('../src/components/Welcome.vue')
+ },
+  {
     path: '/products',
     name: 'Products',
     component: () => import('../src/components/Products.vue')
@@ -10,6 +20,11 @@ const routes = [
     path: '/offers',
     name: 'Offers',
     component: () => import('../src/components/Offers.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: () => import('../src/components/Checkout.vue')
   }
 ]
 
